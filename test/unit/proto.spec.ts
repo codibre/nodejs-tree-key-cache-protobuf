b7b8c8c4b4c956ddff4d696ce234192fcf844f90
Thiago Santos <tos.oliveira@gmail.com>
2023-10-01 09:42:39 -0300

    test: improving proto tests

--- test/unit/proto.spec.ts
+++ test/unit/proto.spec.ts
@@ -1,25 +1,26 @@
-import { Tree, TreeKeys } from 'tree-key-cache';
+import { TreeKeys } from 'tree-key-cache';
 import protobuf from 'protobufjs';
 import { join } from 'path';
+import { gzipSync } from 'zlib';
 
 describe('proto', () => {
 	it('should convert tree to proto and the other way around', async () => {
-		const tree: Tree<Buffer> = {
-			[TreeKeys.value]: Buffer.from([1, 2]),
+		const tree = {
+			[TreeKeys.value]: Buffer.from([1, 2]) as string | Buffer,
 			[TreeKeys.children]: {
 				a: {
-					[TreeKeys.value]: Buffer.from([3, 4]),
+					[TreeKeys.value]: Buffer.from([3, 4]) as string | Buffer,
 					[TreeKeys.children]: {
 						a1: {
-							[TreeKeys.value]: Buffer.from([5, 6]),
+							[TreeKeys.value]: Buffer.from([5, 6]) as string | Buffer,
 						},
 					},
 				},
 				b: {
-					[TreeKeys.value]: Buffer.from([5, 6]),
+					[TreeKeys.value]: Buffer.from([5, 6]) as string | Buffer,
 					[TreeKeys.children]: {
 						b1: {
-							[TreeKeys.value]: Buffer.from([7, 8]),
+							[TreeKeys.value]: Buffer.from([7, 8]) as string | Buffer,
 						},
 					},
 				},
@@ -33,7 +34,15 @@ describe('proto', () => {
 		const decoded = type.toObject(type.decode(encoded));
 
 		expect(tree).toEqual(decoded);
+		tree[TreeKeys.value] = '12';
+		tree[TreeKeys.children].a[TreeKeys.value] = '34';
+		tree[TreeKeys.children].b[TreeKeys.value] = '56';
+		tree[TreeKeys.children].a[TreeKeys.children].a1[TreeKeys.value] = '56';
+		tree[TreeKeys.children].b[TreeKeys.children].b1[TreeKeys.value] = '78';
 		const originalLength = JSON.stringify(tree).length;
 		expect(encoded.length).toBeLessThan(originalLength);
+		const zippedLength = gzipSync(encoded).length;
+		const originalZippedLength = gzipSync(JSON.stringify(tree)).length;
+		expect(zippedLength).toBeLessThan(originalZippedLength);
 	});
 });
